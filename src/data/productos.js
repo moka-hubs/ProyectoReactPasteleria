cf46a4d383b95958756ab57dc0ac7ce01489f196
moka-hubs <joaquinfuenzalida727@gmail.com>
2026-09-24 09:32:59 -0300

    se agrega lista de productos y categorias , junto a clientes iniciales

---
+++ src/data/productos.js
@@ -0,0 +1,57 @@
+export const CATEGORIAS = [
+    "Todos Los Productos",
+    "Productos Sin Azúcar",
+    "Tortas Cuadradas",
+    "Tortas Circulares",
+    "Postres Individuales",
+    "Pasteleria Tradicional",
+    "Productos Sin Gluten",
+    "Productos Veganos",
+    "Tortas Especiales",
+    "Más Vendidos"
+]
+
+export const productosIniciales = [
+  { codigo: "TC001", imagen: "tortacuadradachocolate.png", categoria: "Tortas Cuadradas", nombre: "Torta Cuadrada de Chocolate", precio: 45000, stock: 8, descripcion: "Deliciosa torta de chocolate con capas de ganache y un toque de avellanas personalizable con mensajes especiales." },
+  { codigo: "TC002", imagen: "tortacuadradafrutas.png", categoria: "Tortas Cuadradas", nombre: "Torta Cuadrada de Frutas", precio: 50000, stock: 12, descripcion: "Una mezcla de frutas frescas y crema chantilly sobre un suave bizcocho de vainilla, ideal para celebraciones." },
+  { codigo: "TC003", imagen: "tortasanjorge.png", categoria: "Tortas Cuadradas", nombre: "Torta San Jorge", precio: 27990, stock: 15, descripcion: "Postre tradicional chileno que destaca por su perfecta combinación de texturas, masa de hojarasca con manjar y lúcuma." },
+  { codigo: "TT001", imagen: "tortacircularvainilla.png", categoria: "Tortas Circulares", nombre: "Torta Circular de Vainilla", precio: 40000, stock: 10, descripcion: "Bizcocho de vainilla clásico relleno con crema pastelera y cubierto con un glaseado dulce, perfecto para cualquier ocasión." },
+  { codigo: "TT002", imagen: "tortacircularmanjar.png", categoria: "Tortas Circulares", nombre: "Torta Circular de Manjar", precio: 42000, stock: 7, descripcion: "Torta tradicional chilena con manjar y nueces, un deleite para los amantes de los sabores dulces y clásicos." },
+  { codigo: "PI001", imagen: "moussechocolate.png", categoria: "Postres Individuales", nombre: "Mousse de Chocolate", precio: 5000, stock: 25, descripcion: "Postre individual cremoso y suave, hecho con chocolate de alta calidad, ideal para los amantes del chocolate." },
+  { codigo: "PI002", imagen: "tiramisu.png", categoria: "Postres Individuales", nombre: "Tiramisú Clásico", precio: 5500, stock: 18, descripcion: "Un postre italiano individual con capas de café, mascarpone y cacao, perfecto para finalizar cualquier comida." },
+  { codigo: "PSA001", imagen: "tortanaranjasinazucar.png", categoria: "Productos Sin Azúcar", nombre: "Torta Sin Azúcar de Naranja", precio: 48000, stock: 6, descripcion: "Torta ligera y deliciosa, endulzada naturalmente, ideal para quienes buscan opciones más saludables." },
+  { codigo: "PSA002", imagen: "cheesecakesinazucar.png", categoria: "Productos Sin Azúcar", nombre: "Cheesecake Sin Azúcar", precio: 47000, stock: 9, descripcion: "Suave y cremoso, este cheesecake es una opción perfecta para disfrutar sin culpa." },
+  { codigo: "PT001", imagen: "empanadamanzana.png", categoria: "Pastelería Tradicional", nombre: "Empanada de Manzana", precio: 3000, stock: 40, descripcion: "Pastelería tradicional rellena de manzanas especiadas, perfecta para un dulce desayuno o merienda." },
+  { codigo: "PT002", imagen: "tartasantiago.png", categoria: "Pastelería Tradicional", nombre: "Tarta de Santiago", precio: 6000, stock: 22, descripcion: "Tradicional tarta española hecha con almendras, azúcar, y huevos, una delicia para los amantes de los postres clásicos." },
+  { codigo: "PG001", imagen: "browniesingluten.png", categoria: "Productos Sin Gluten", nombre: "Brownie Sin Gluten", precio: 4000, stock: 30, descripcion: "Rico y denso, este brownie es perfecto para quienes necesitan evitar el gluten sin sacrificar el sabor." },
+  { codigo: "PG002", imagen: "pansingluten.png", categoria: "Productos Sin Gluten", nombre: "Pan Sin Gluten", precio: 3500, stock: 20, descripcion: "Suave y esponjoso, ideal para sándwiches o para acompañar cualquier comida." },
+  { codigo: "PV001", imagen: "tortaveganachocolate.png", categoria: "Productos Veganos", nombre: "Torta Vegana de Chocolate", precio: 50000, stock: 5, descripcion: "Torta de chocolate húmeda y deliciosa, hecha sin productos de origen animal, perfecta para veganos." },
+  { codigo: "PV002", imagen: "galletasveganasavena.png", categoria: "Productos Veganos", nombre: "Galletas Veganas de Avena", precio: 4500, stock: 28, descripcion: "Crujientes y sabrosas, estas galletas son una excelente opción para un snack saludable y vegano." },
+  { codigo: "TE001", imagen: "tortacumple.png", categoria: "Tortas Especiales", nombre: "Torta Especial de Cumpleaños", precio: 55000, stock: 4, descripcion: "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos." },
+  { codigo: "TE002", imagen: "tortaboda.png", categoria: "Tortas Especiales", nombre: "Torta Especial de Boda", precio: 60000, stock: 2, descripcion: "Elegante y deliciosa, esta torta está diseñada para ser el centro de atención en cualquier boda." },
+  { codigo: "TC004", imagen: "tortatrufa.png", categoria: "Tortas Cuadradas", nombre: "Torta Cuadrada de Trufa", precio: 46990, stock: 11, descripcion: "Intensa torta cuadrada con un suave bizcocho de chocolate relleno de abundante trufa artesanal y finas notas de cacao." },
+  { codigo: "TT003", imagen: "tortacirculartresleches.png", categoria: "Tortas Circulares", nombre: "Torta Circular Tres Leches", precio: 38990, stock: 13, descripcion: "Clásico bizcocho empapado en nuestra mezcla especial de tres leches, cubierto con un delicado merengue italiano." },
+  { codigo: "PI003", imagen: "suspirolimena.png", categoria: "Postres Individuales", nombre: "Suspiro Limeño", precio: 4800, stock: 17, descripcion: "Tradicional postre envasado en vasito individual, con una suave base de manjar blanco y coronado con merengue al oporto." },
+  { codigo: "PSA003", imagen: "muffinsinazucar.png", categoria: "Productos Sin Azúcar", nombre: "Muffin Integral de Arándanos Sin Azúcar", precio: 2500, stock: 35, descripcion: "Esponjoso muffin horneado con harina integral y endulzado con tagatosa, repleto de arándanos frescos." },
+  { codigo: "PT003", imagen: "kuchenframbuesa.png", categoria: "Pastelería Tradicional", nombre: "Kuchen Alemán de Frambuesa", precio: 2990, stock: 14, descripcion: "Tradicional pastel de repostería sureña con una base de masa noble, relleno de jugosas frambuesas y un toque de migas crujientes." },
+  { codigo: "PG003", imagen: "alfajorsingluten.png", categoria: "Productos Sin Gluten", nombre: "Alfajor Artesanal Sin Gluten", precio: 2200, stock: 45, descripcion: "Delicadas tapitas crujientes libres de gluten unidas por un generoso relleno de manjar y sutilmente bañadas en chocolate." },
+  { codigo: "PV003", imagen: "quequeganachevegano.png", categoria: "Productos Veganos", nombre: "Queque Rústico de Plátano y Nuez Vegano", precio: 3500, stock: 16, descripcion: "Queque húmedo y aromático elaborado 100% con ingredientes vegetales, plátanos maduros seleccionados y trozos de nueces." },
+  { codigo: "TE003", imagen: "tortaaniversario.png", categoria: "Tortas Especiales", nombre: "Torta de Aniversario Corazón", precio: 58000, stock: 3, descripcion: "Hermosa torta con forma de corazón decorada con detalles florales en crema y un sabor especial a elección para celebrar el amor." },
+  { codigo: "MV001", imagen: "cheesecakefrutosrojos.png", categoria: "Más Vendidos", nombre: "Cheesecake de Frutos Rojos", precio: 42000, stock: 10, descripcion: "Nuestro clásico cheesecake de textura suave y cremosa, coronado con una generosa capa de salsa artesanal de frambuesas y moras frescas." },
+  { codigo: "MV002", imagen: "kuchenmanzana.png", categoria: "Más Vendidos", nombre: "Kuchen de Manzana Crujiente", precio: 28900, stock: 19, descripcion: "Receta tradicional de la zona sur, con finas láminas de manzana acarameladas sobre una masa suave y cubierta de migas crujientes." },
+  { codigo: "MV003", imagen: "volcan.png", categoria: "Más Vendidos", nombre: "Volcán de Chocolate Belga", precio: 6500, stock: 24, descripcion: "Delicioso pastelito individual de chocolate con un núcleo fundente y cálido que se desborda al primer corte. Ideal para acompañar con helado." },
+  { codigo: "MV004", imagen: "tortamanjarlucuma.png", categoria: "Más Vendidos", nombre: "Torta Hojarasca Manjar Lúcuma", precio: 44000, stock: 8, descripcion: "Una de las favoritas de la casa: capas ultrafinas y crujientes de hojarasca rellenas de manjar de leche condensada y crema ligera de lúcuma." },
+  { codigo: "MV005", imagen: "cajaalfajor.png", categoria: "Más Vendidos", nombre: "Caja de 6 Alfajores Artesanales", precio: 7500, stock: 33, descripcion: "Exquisitos alfajores rellenos con abundante manjar casero, bañados en cobertura de chocolate bitter y con bordes de coco tostado." },
+  { codigo: "MV006", imagen: "pielimon.png", categoria: "Más Vendidos", nombre: "Pie de Limón Con Merengue", precio: 26000, stock: 12, descripcion: "Crocante masa de tarta con una crema de limón fresca y equilibrada, coronada con copitos de merengue italiano ligeramente dorado." },
+  { codigo: "MV007", imagen: "cinnamon.png", categoria: "Más Vendidos", nombre: "Cinnamon Rolls Glaseados (Pack x4)", precio: 6990, stock: 27, descripcion: "Esponjosos rollitos de canela recién horneados, cubiertos con un dulce y suave glaseado de queso crema que se derrite en la boca." },
+  { codigo: "MV008", imagen: "redvelvet.png", categoria: "Más Vendidos", nombre: "Torta Red Velvet Clásica", precio: 49000, stock: 6, descripcion: "Elegante bizcocho rojo de cacao suave con sutiles notas de vainilla, intercalado y cubierto con una exquisita crema de queso crema." }
+];
+
+export const clientesIniciales = [
+    {nombre : "Marcelo Dervis",correo:"marcelo3678@gmail.com"},
+    {nombre : "Paulo Riveros" , correo : "paulo2183418@gmail.com"},
+    {nombre : "Joaquin Fuenzalida",correo : "joaquin16235167@gmail.com"}
+]
+
+
+
